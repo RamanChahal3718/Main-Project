ff370f3863ccc07b835a561ff0424d8fb79e16b7
Raman <ramanchahal3718@gmail.com>
2023-04-13 21:35:08 -0400

    uploaded files, theme name is mainproject

---
+++ themes/cstmtheme/footer.php
@@ -0,0 +1,16 @@
+<footer>
+		<div class="col-sm-12 col-md-3 col-lg-3">
+			<?php dynamic_sidebar( 'footer_area_one' ) ?>
+		</div>
+		<div class="col-sm-12 col-md-3 col-lg-3">
+		   <?php dynamic_sidebar( 'footer_two_one' ) ?>
+		</div>
+		<div class="col-sm-12 col-md-3 col-lg-3">
+		   <?php dynamic_sidebar( 'footer_three_one' ) ?>
+		</div>
+		<div class="col-sm-12 col-md-3 col-lg-3">
+		   <?php dynamic_sidebar( 'footer_four_one' ) ?>
+		</div>
+	</footer>
+  </body>
+</html>
